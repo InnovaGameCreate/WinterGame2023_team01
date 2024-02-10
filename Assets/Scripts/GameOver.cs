1ef9f264023b9095db6ede0b043b5749b896b53c
yuuuuuuuz <fff.yuzu.3156@gmail.com>
2024-02-10 17:44:15 +0900

    Update GameOver.cs 3

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -12,7 +12,7 @@ public class GameOver : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-      // プレハブでResult表示
+      // Canvasプレハブをアクティブ
       CanvasPrefab.gameObject.SetActive(true);
       // シーン切り替えでResult表示
       // SceneManager.LoadScene("Result");
