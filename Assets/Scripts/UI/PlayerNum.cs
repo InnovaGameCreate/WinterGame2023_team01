f71d7216325988e7ff40d2327c383d6c3112694f
avocado <ssp.10.01e@gmail.com>
2024-02-11 22:25:08 +0900

    title
    人数変更ui

--- Assets/Scripts/UI/PlayerNum.cs
+++ Assets/Scripts/UI/PlayerNum.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;  
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerNum : MonoBehaviour
 {
     
-    public GameObject num= null; // Textオブジェクト
+    public GameObject num = null; // Textオブジェクト
+    [SerializeField] public int player_max_num;
     public int player_num = 0; // 変数
     [SerializeField] string playerText;
+    private TextMeshProUGUI Player_text;
 
     // Update is called once per frame
+    private void Start()
+    {
+        // オブジェクトからTextコンポーネントを取得
+        Player_text = num.GetComponent<TextMeshProUGUI>();
+    }
     void Update()
     {
-         // オブジェクトからTextコンポーネントを取得
-        Text Player_text = num.GetComponent<Text> ();
-             // テキストの表示を入れ替える
-     Player_text.text = playerText + ":" + player_num;
+       
+        // テキストの表示を入れ替える
+        Player_text.text = playerText + "：" + player_num;
 
     }
 }
